04dfa8c554bec7e495e793c7b9a4ce8f29472678
nah22 <nadeem@acm.org>
2010-10-25 23:38:06 -0400

    added kiva-data->structs function, per Shriram

--- kiva-to-struct.rkt
+++ kiva-to-struct.rkt
@@ -1,8 +1,10 @@
-#lang racket
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname kiva-to-struct) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
 
 (require "kiva-teachpack.rkt")
 
-(define-struct loan (borrower amount year) #:transparent)
+(define-struct loan (borrower amount year))
 
 (kiva-data->structs      ;; function from the teachpack
  sample-kiva-data        ;; this is the row of data
